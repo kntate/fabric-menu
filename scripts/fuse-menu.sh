c48ac8a0fdd897b5cb049554e8546d779374f39d
Kevin <kevin@kevin-ThinkPad-T530.(none)>
2014-11-06 13:34:21 -0600

    blocking admin option if no containers are installed

--- scripts/fuse-menu.sh
+++ scripts/fuse-menu.sh
@@ -57,113 +57,121 @@ do
 	echo -e "\n${mainTitle}\n"
 	;;
     "administration")
-	admin_title="\E[0;33;41m\033[1m#### Main Menu - $chosen_application - $chosen_environment > Administration ####\033[0m"
-	echo -e "\n${admin_title}\n"
-	select admin_menu in "sshToContainer" "containerUpgrade" "containerRollback" "startContainer" "stopContainer" "addProfile" "removeProfile" "environmentInfo" "containerStatus" "camel" "activeMQStats" "threadDump" "containerConnect" "rootMenu"
-	do
-	  echo "$admin_menu";
-	  case $admin_menu in	    
-	  "sshToContainer")
-		sshToContainer
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;	
-	    "containerUpgrade")
-		containerUpgrade
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;	
-	    "containerRollback")
-		containerRollback
+    
+	# Make sure there are containers in the environment
+	getAllContainerList
+	number_containers=${#container_array[@]}
+	if [ $number_containers -lt 1 ]; then
+	  echo "Error, no applications installed for application $chosen_application and environment $chosen_environment. Use installApp to install first."
+	else
+	  admin_title="\E[0;33;41m\033[1m#### Main Menu - $chosen_application - $chosen_environment > Administration ####\033[0m"
+	  echo -e "\n${admin_title}\n"
+	  select admin_menu in "sshToContainer" "containerUpgrade" "containerRollback" "startContainer" "stopContainer" "addProfile" "removeProfile" "environmentInfo" "containerStatus" "camel" "activeMQStats" "threadDump" "containerConnect" "rootMenu"
+	  do
+	    echo "$admin_menu";
+	    case $admin_menu in	    
+	    "sshToContainer")
+		  sshToContainer
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;	
+	      "containerUpgrade")
+		  containerUpgrade
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;	
+	      "containerRollback")
+		  containerRollback
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;
+	      "startContainer")
+		  startContainer
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;	
+	      "stopContainer")
+		  stopContainer
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;		
+	      "addProfile")
+		  addProfile
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;
+	      "environmentInfo")
+		  environmentInfo
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;
+	      "removeProfile")
+		  removeProfile
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;	
+	      "containerStatus")
+		  containerStatus
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;		
+	      "camel")
+		camel_title="\E[0;33;44m\033[1m#### Main Menu - $chosen_application - $chosen_environment > Administration > Camel ####\033[0m"
+		echo -e "\n${camel_title}\n"	    
+		select camel_menu in "camelRouteStart" "camelRouteStop" "camelRouteInfo" "backToAdminMenu"
+		do
+		  echo "$camel_menu";
+		  case $camel_menu in		    
+		  "camelRouteStart")
+		      camelRouteStart
+		      REPLY=""
+		      echo -e "\n${camel_title}\n"
+		      ;;	
+		  "camelRouteStop")
+		      camelRouteStop
+		      REPLY=""
+		      echo -e "\n${camel_title}\n"
+		      ;;
+		  "camelRouteInfo")
+		      camelRouteInfo
+		      REPLY=""
+		      echo -e "\n${camel_title}\n"
+		      ;;  
+		  "backToAdminMenu")
+		      break;
+		      ;;		  
+		  *)
+		      echo "That is not a valid choice!  Input a number."
+		      ;;			    
+		  esac	  
+		done	
 		REPLY=""
 		echo -e "\n${admin_title}\n"
 		;;
-	    "startContainer")
-		startContainer
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;	
-	    "stopContainer")
-		stopContainer
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;		
-	    "addProfile")
-		addProfile
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;
-	    "environmentInfo")
-		environmentInfo
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;
-	    "removeProfile")
-		removeProfile
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;	
-	    "containerStatus")
-		containerStatus
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;		
-	    "camel")
-	      camel_title="\E[0;33;44m\033[1m#### Main Menu - $chosen_application - $chosen_environment > Administration > Camel ####\033[0m"
-	      echo -e "\n${camel_title}\n"	    
-	      select camel_menu in "camelRouteStart" "camelRouteStop" "camelRouteInfo" "backToAdminMenu"
-	      do
-		echo "$camel_menu";
-		case $camel_menu in		    
-		"camelRouteStart")
-		    camelRouteStart
-		    REPLY=""
-		    echo -e "\n${camel_title}\n"
-		    ;;	
-		"camelRouteStop")
-		    camelRouteStop
-		    REPLY=""
-		    echo -e "\n${camel_title}\n"
-		    ;;
-		"camelRouteInfo")
-		    camelRouteInfo
-		    REPLY=""
-		    echo -e "\n${camel_title}\n"
-		    ;;  
-		"backToAdminMenu")
-		    break;
-		    ;;		  
-		*)
-		    echo "That is not a valid choice!  Input a number."
-		    ;;			    
-		esac	  
-	      done	
-	      REPLY=""
-	      echo -e "\n${admin_title}\n"
-	      ;;
-	    "activeMQStats")
-		activeMQStats
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;	
-	    "threadDump")
-		threadDump
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;			
-	    "containerConnect")
-		containerConnect
-		REPLY=""
-		echo -e "\n${admin_title}\n"
-		;;			  
-	  "rootMenu")
+	      "activeMQStats")
+		  activeMQStats
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;	
+	      "threadDump")
+		  threadDump
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;			
+	      "containerConnect")
+		  containerConnect
+		  REPLY=""
+		  echo -e "\n${admin_title}\n"
+		  ;;	
+	 "rootMenu")
 	      break;
 	      ;;
 	  *)
 	      echo "That is not a valid choice!  Input a number."
 	      ;;	
 	  esac	  
-	done
+	done		  
+	fi
 	REPLY=""
 	echo -e "\n${mainTitle}\n"
 	;;	
